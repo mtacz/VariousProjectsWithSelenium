e7477ffc75bfd4f7864ab6ad73a70cbcf6ee0f87
mtacz <magdalena.taczuk+github@gmail.com>
2026-01-17 23:42:57 +0100

    @Parameters

--- src/test/java/page/objects/LoginPage.java
+++ src/test/java/page/objects/LoginPage.java
@@ -1,4 +1,38 @@
 package page.objects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class LoginPage {
+
+    WebDriver driver;
+
+    private By userName = By.id("username");
+    private By fieldPassword = By.id("password");
+    private By loginButton = By.cssSelector("button.radius");
+    private By expectedWarning = By.id("flash");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openPage() {
+        driver.navigate().to("https://theinternet.przyklady.javastart.pl/login");
+    }
+
+    public void login(String username, String password){
+        driver.findElement(userName).sendKeys(username);
+        driver.findElement(fieldPassword).sendKeys(password);
+        driver.findElement(loginButton).click();
+    }
+
+    public String getExpectedWarningMessage(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(expectedWarning)).getText();
+    }
+
 }
--- src/test/java/testng/practice/tests/LoginTests.java
+++ src/test/java/testng/practice/tests/LoginTests.java
@@ -1,2 +1,42 @@
+package testng.practice.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import page.objects.LoginPage;
+
 public class LoginTests {
+
+    private WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void beforeTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        loginPage = new LoginPage(driver);
+    }
+
+    @Parameters({"userName","password","expectedWarning"})
+    @Test
+    public void shouldFailLogin(String userName, String password, String expectedWarning) {
+        loginPage.openPage();
+        loginPage.login(userName,password);
+
+        String error = loginPage.getExpectedWarningMessage();
+        Assert.assertTrue(error.contains(expectedWarning));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
